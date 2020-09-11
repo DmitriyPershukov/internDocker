9266c03e78f14eeffa7fa5480a8050ebcc5bf799
DmitriyPershukov <dmitriypershukov@gmail.com>
2020-09-11 08:14:16 +0500

    Update urls.py

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('categories', views.CategoriesView.as_view(), name='categories'),
     path('levels', views.LevelsView.as_view(), name='levels'),
     path('themes', views.ThemesView.as_view(), name='themes'),
-    path('themes/<int:id>/', views.ThemesView.as_view(), name='themes'),
-    path('words/<int:id>/', views.WordView.as_view(), name='words')
+    path('themes/<int:id>', views.ThemesView.as_view(), name='themes'),
+    path('words/<int:id>', views.WordView.as_view(), name='words')
 ]
