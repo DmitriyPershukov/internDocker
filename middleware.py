40e69bf42be0a67bc50b8938d5dfd2199ae9886a
DmitriyPershukov <dmitriypershukov@gmail.com>
2021-01-30 13:16:37 +0500

    decorator date

--- middleware.py
+++ middleware.py
@@ -6,8 +6,18 @@ class APISecretMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+
     def __call__(self, request):
-        if((not ('Secret' in request.headers) or request.headers["Secret"] != settinds.API_SECRET)):
+        if ((not ('Secret' in request.headers) or request.headers["Secret"] != settinds.API_SECRET)):
+
             return HttpResponseForbidden()
         response = self.get_response(request)
+        # Code to be executed for each request/response after
+        # the view is called.
         return response
+
+    def process_request(self, request):
+        if ((not ('Secret' in request.headers) or request.headers["Secret"] != settinds.API_SECRET)):
+
+            return HttpResponseForbidden()
+        return None
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,26 +1,37 @@
 
 
 from django.http import HttpResponse
+from django.views.generic import ListView
+
 import vocabulary.models, vocabulary.customserializers
 import vocabulary.customserializers
 from django.views import View
 from django.apps import apps
 from django.utils.module_loading import import_string
 from django.utils.decorators import decorator_from_middleware, decorator_from_middleware_with_args
+
+from django.utils.decorators import method_decorator
 from middleware import APISecretMiddleware
 
 
 argsmiddleware = decorator_from_middleware_with_args(APISecretMiddleware)
-class CategoriesView(View):
-    @decorator_from_middleware(APISecretMiddleware)
+
+
+class APIViewMixin(View):
+    apiMiddleware = decorator_from_middleware(APISecretMiddleware)
+    @method_decorator(APISecretMiddleware)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch( *args, **kwargs)
+
+class CategoriesView(APIViewMixin, View):
+
     def get(self, request, *args, **kwargs):
-        serializer = vocabulary.customserializers.CategorySerialiser();
-        return HttpResponse(serializer.serialize(vocabulary.models.Categories.objects.all()), content_type='application/json')
-class ThemesView(View):
+        serializer = vocabulary.customserializers.CategorySerialiser();b
 
-    @decorator_from_middleware(APISecretMiddleware)
+        return HttpResponse(serializer.serialize(vocabulary.models.Categories.objects.all()),content_type='application/json')
+
+class ThemesView(APIViewMixin, View):
     def get(self, request, id= None, *args, **kwargs):
-
         if (id!= None):
             serializer = vocabulary.customserializers.ThemesSerialiser(showWords=True);
             return HttpResponse(serializer.serialize([vocabulary.models.Theme.objects.get(pk= id)]), content_type='application/json')
@@ -28,23 +39,15 @@ class ThemesView(View):
             serializers = vocabulary.customserializers.ThemesSerialiser(showWords=False);
             return HttpResponse(serializers.serialize(vocabulary.models.Theme.objects.all()), content_type='application/json',)
 
-class LevelsView(View):
-    @decorator_from_middleware(APISecretMiddleware)
+class LevelsView(APIViewMixin,View):
     def get(self, request, *args, **kwargs):
-
         serializers = vocabulary.customserializers.LevelsSerialiser();
         responce = HttpResponse();
         responce.write(serializers.serialize(vocabulary.models.Level.objects.all()))
-        return HttpResponse(serializers.serialize(vocabulary.models.Level.objects.all()),content_type='application/json');
+        return HttpResponse(serializers.serialize(vocabulary.models.Level.objects.all()), content_type='application/json');
 
-class WordView(View):
-    @decorator_from_middleware(APISecretMiddleware)
+class WordView(APIViewMixin,View):
+
     def get(self,request, id,*args, **kwargs):
         serializers=vocabulary.customserializers.WordSerialiser()
         return HttpResponse(serializers.serialize([vocabulary.models.Word.objects.get(pk=id)]), content_type='application/json')
-
-class AdminView(View):
-    @decorator_from_middleware(APISecretMiddleware)
-    def _setup(self):
-        AdminSiteClass = import_string(apps.get_app_config('admin').default_site)
-        self._wrapped = AdminSiteClass()
